c6a9b88622ce77b0745a46328894a7295a3d5298
Anuj Shrestha <anuj@sgt.com.np>
2015-08-23 11:44:04 +0545

    Remove show and index view urls

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,8 @@
 class ReviewsController < ApplicationController
-  before_filter :set_review, only: [:show, :edit, :update, :destroy]
+  before_filter :set_review, only: [:edit, :update, :destroy]
   before_filter :authenticate_user!
 
   respond_to :html
-
-  def index
-    @reviews = Review.all
-    respond_with(@reviews)
-  end
-
-  def show
-    respond_with(@review)
-  end
 
   def new
     @review = Review.new
@@ -24,14 +15,31 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(params[:review])
     @review.user_id = current_user.id
-    @review.save
-    respond_with(@review)
+     respond_to do |format|
+      if @review.save
+        format.html { redirect_to root_path, notice: "Review was successfully created." }
+        format.json { render :show, status: :created, location: @review }
+      else
+        format.html { render :new }
+        format.json { render json: @review.errors, status: :unprocessable_entity}
+        
+      end
   end
+end
 
   def update
-    @review.update_attributes(params[:review])
-    respond_with(@review)
+    respond_to do |format|
+       if @review.update{review_params}
+         format.html { redirect_to @review, notice: "Review was successfully updated."}
+         format.json { render :show, status: :ok, location: @review }
+      else
+         format.html { render :edit }
+         format.json { render json: @review.errors, status: :unprocessable_entity }
+
   end
+end
+end
+
 
   def destroy
     @review.destroy
